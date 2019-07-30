f65386b3aeb121e1001cf911099da9e642f7e75c
svallejos <sebastian.vallejos+github@isistan.unicen.edu.ar>
2019-07-30 12:16:03 -0300

    Arreglo en el height de las etapas

--- Vue.js
+++ Vue.js
@@ -57,45 +57,42 @@ var app = new Vue({
   
   template: `
   <div> 
-  <div class="header"> 
-      {{message}}
-      <img class="image" v-bind:src="image" />  
-  </div>
-  <div class="imputs">
-    <label for="nroproc">Numero de procesadores:</label>
-    <input type="number" v-model= "nroproc" placeholder="multiplo de 2" step="2" min="0" style="width:100px">
-    <label for="nroproc">Procesadores activos:</label>
-    <input type="number" v-model= "nroprocactivos" placeholder="multiplo de 2" step="2" min="0" style="width:100px">
-    <span>Periodicidad de los requerimientos</span>
-    <select v-model="periodicidad">
-    <option>Periodico</option>
-    <option>Unica vez</option>
-    <option>Al azar</option>
-    </select>
-
-    <span>Configuracion de direccion de memoria</span>
-    <select v-model="configuracion">
-    <option>Direccion unica</option>
-    <option>Direcciones al azar</option>
-    <option>Lista de direcciones</option>
-    </select>
-
-  </div>
-
-  
-  
-   
-  
-  <div class="container-fluid">
-  <div class="row justify-content-center">
-    <div class="col">      
-        <procesador v-for= "po in procesadores" v-bind:procesador=po> </procesador>
-      </div>
-      <div v-for= "eo in etapas" class="col">
-      <etapa v-bind:etapa=eo> </etapa>
+    <div class="header"> 
+        {{message}}
+        <img class="image" v-bind:src="image" />  
     </div>
-  </div>
-</div>
+    <div class="imputs">
+      <label for="nroproc">Numero de procesadores:</label>
+      <input type="number" v-model= "nroproc" placeholder="multiplo de 2" step="2" min="0" style="width:100px"></input>
+      <label for="nroproc">Procesadores activos:</label>
+      <input type="number" v-model= "nroprocactivos" placeholder="multiplo de 2" step="2" min="0" style="width:100px"></input>
+      <span>Periodicidad de los requerimientos</span>
+      <select v-model="periodicidad">
+        <option>Periodico</option>
+        <option>Unica vez</option>
+        <option>Al azar</option>
+      </select>
+
+      <span>Configuracion de direccion de memoria</span>
+      <select v-model="configuracion">
+        <option>Direccion unica</option>
+        <option>Direcciones al azar</option>
+        <option>Lista de direcciones</option>
+      </select>
+    </div>
+    
+    <div class="container">
+      <div class="row display-flex">
+        <div class="col my-col">      
+          <procesador v-for= "po in procesadores" v-bind:procesador=po> 
+          </procesador>
+        </div>
+        <div v-for= "eo in etapas" class="col my-col">
+          <etapa v-bind:etapa=eo>
+          </etapa>
+        </div>
+      </div>
+    </div>
 
   </div>`
 
